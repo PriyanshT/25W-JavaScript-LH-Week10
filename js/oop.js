3217825a8b8308b6b1674b427bf1a1f189ab3c69
Priyansh Thakar <priyanshthakar@gmail.com>
2025-03-10 19:45:06 -0400

    oop complete

--- js/oop.js
+++ js/oop.js
@@ -2,11 +2,29 @@ const output = document.getElementById('output');
 const output2 = document.getElementById('output2');
 
 /* STEP 1a: Create a new object using a regular function */
+function createNewPerson(name) {
+    let obj = {};
+    obj.name = name;
+    obj.greetings = function () {
+        alert(`Hey, this is ${this.name}.`);
+    };
+    return obj;
+}
 
 /* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+let person1 = createNewPerson("P1");
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
 rewrite the above function, without returning anything. Capitalize the name of the function. */
+function Person(firstName, lastName, age, interests) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.interests = interests;
+    this.bio = function () {
+        return `The interests of ${this.firstName} ${this.lastName}, of age ${this.age} are ${this.interests}.`;
+    }
+}
 
 /* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
 and again invoking the .greeting() method for each person */
@@ -15,20 +33,30 @@ and again invoking the .greeting() method for each person */
  Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
 
 /* STEP 3b: Instantiate a new Person based on the above constructor */
+let person2 = new Person("P1", "LP1", 18, ["swimming", "reading", "dancing"]);
 
 /* STEP 3c: Attempt to access the various properties of person1 using the console. */
-// person1['age']
-// person1.interests[1]
-// person1.bio()
+// person2['age']
+// person2.interests[1]
+// person2.bio()
+output.textContent = person2.bio();
 
 /* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+let car = new Object();
 
 /* STEP 4b: Once 'car' is created, add various properties and methods… */
+car.brand = "Honda";
+car.model = "Civic";
+car.fun = function () {
+    alert(`A car of brand: ${this.brand} and model: ${this.model}.`);
+}
 
 /* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
 
 /* STEP 5a: Yet another approach is to use the create() method. 
 Let's see how the above car object can be used to create another object */
+let anotherCar = Object.create(car);
+anotherCar.model = "CRV";
 
 /* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
 
